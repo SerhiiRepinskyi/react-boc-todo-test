b5d83a9f1eff3c56588bf27293082db5bdc55728
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-03-02 22:09:10 +0200

    feat: added AppBar with counters

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,10 +1,28 @@
-export const AppBar = () => {
+export const AppBar = ({ todos }) => {
+  const count = todos.reduce(
+    (acc, todo) => {
+      if (todo.completed) {
+        acc.completed += 1
+      } else {
+        acc.active += 1
+      }
+      return acc
+    },
+    { active: 0, completed: 0 }
+  )
+
   return (
     <div className="mb-2 flex h-24 flex-col justify-center gap-1">
       <h2 className="font-bold">Tasks</h2>
       <div className="flex flex-col">
-        <p>Active: 0</p>
-        <p>Completed: 0</p>
+        <p>
+          Active:{' '}
+          <span className="bg-gray-300 px-1.5 py-px">{count.active}</span>
+        </p>
+        <p>
+          Completed:{' '}
+          <span className="bg-gray-300 px-1.5 py-px">{count.completed}</span>
+        </p>
       </div>
     </div>
   )
